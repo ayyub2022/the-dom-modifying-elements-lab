6a04705e00cb8bf8e9d6dd195d19f2f24864b5fe
ayyub2022 <ayyubibrahim22@gmail.com>
2024-04-15 14:23:19 +0300

    first commit

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
+
 // Write your code here!
+
+const mainNode = document.querySelector('main#main')
+mainNode.remove()
+
+let newHeader = document.createElement("h1")
+
+newHeader.setAttribute("id", "victory")
+
+newHeader.textContent = "Ayub is the champion"
